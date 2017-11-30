910200f73710bcf255f5381c5b07da70e57a3691
v-gmahajan <v-gmahajan@shutterfly.com>
2017-11-30 10:52:07 +0530

    module changes and gitIgnore

--- src/app/lib/test/test-lib.module.ts
+++ src/app/lib/test/test-lib.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TestComponent } from './test.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  exports:[TestComponent],
   declarations: []
 })
 export class TestLibModule { }
